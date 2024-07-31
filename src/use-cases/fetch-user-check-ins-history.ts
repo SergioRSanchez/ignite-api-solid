1ab9c2e7244555c86d08fde9c75d02705d5fb4ae
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-07-31 16:42:31 -0300

    feat: include pagination

--- src/use-cases/fetch-user-check-ins-history.ts
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -4,6 +4,7 @@ import { CheckInsRepository } from '@/repositories/check-ins-repository'
 
 interface FetchUserCheckInHistoryUseCaseRequest {
   userId: string
+  page: number
 }
 
 interface FetchUserCheckInHistoryUseCaseResponse {
@@ -16,8 +17,12 @@ export class FetchUserCheckInHistoryUseCase {
 
   async execute({
     userId,
+    page,
   }: FetchUserCheckInHistoryUseCaseRequest): Promise<FetchUserCheckInHistoryUseCaseResponse> {
-    const checkIns = await this.checkInsRepository.findManyByUserId(userId)
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page,
+    )
 
     return {
       checkIns,
