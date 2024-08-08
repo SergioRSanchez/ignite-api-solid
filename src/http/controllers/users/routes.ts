f351375ac4d7d28326a2e2455057761bba163723
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-08-08 17:54:03 -0300

    feat: changed function name

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -6,7 +6,7 @@ import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
 
-export async function appRoutes(app: FastifyInstance) {
+export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
